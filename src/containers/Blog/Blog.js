ea5bc68b4078a4612f1529c9abcc40b436f43e8f
Colm <colm29@gmail.com>
2020-05-11 22:12:55 +0100

    switch between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import { Route } from 'react-router-dom'
 
-
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from '../../containers/Blog/NewPost/NewPost'
 
 class Blog extends Component {
     render () {
@@ -21,6 +21,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/"  render={() => <h1>Home2</h1>} />*/}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
