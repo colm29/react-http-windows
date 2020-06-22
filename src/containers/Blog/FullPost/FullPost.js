b72e4604160c901f4c38cdd36eb8efeded47df08
Colm <colm29@gmail.com>
2020-06-22 22:41:25 +0100

    dynamic nested route with componentDidUpdate

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -9,9 +9,13 @@ class FullPost extends Component {
     };
 
     componentDidMount() {
+        this.loadData()
+    }
+
+    loadData() {
         console.log(this.props)
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
                 axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         //console.log(response)
@@ -21,8 +25,12 @@ class FullPost extends Component {
         }
     }
 
+    componentDidUpdate() {
+        this.loadData()
+    }
+
     deletePostHandler = () => {
-        axios.delete('posts/' + this.props.id)
+        axios.delete('posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response)
             }   
@@ -31,7 +39,7 @@ class FullPost extends Component {
 
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{ textAlign: 'center' }}>Loading...</p>;
         }
         if (this.state.loadedPost) {
